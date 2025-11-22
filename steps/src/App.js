0034079690052bf408385665e71a63726482d6c3
achrefmechergui <achreffcb123achref@gamil.com>
2025-11-22 21:15:31 +0100

    state-done

--- steps/src/App.js
+++ steps/src/App.js
@@ -7,73 +7,70 @@ const messages = [
 ];
 
 function App() {
+  let [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);   //// 
 
-   let [step , setStep] = useState(1)
-   const [test ,setTest] = useState({name:"ali"})   
-  
+  function handlePrevious() {
+    if (step > 1) setStep((current)=> current - 1);
+  }
 
+  function handleNext() {
+    if (step < 3) {
+     setStep((current)=> current + 1)
 
-   function handlePrevious (){
-    
-     if (step > 1)  setStep(step - 1)
+    }
+   
 
-      
-
-   }
-
-   function handleNext (){
-     if (step < 3 )  setStep (step + 1 )
-
-       
-      ///// bad practice ::: 
-      // test.name = "mohamed"
-      // setTest ({name:"peter"})
-   }
-
+    ///// bad practice :::
+    // test.name = "mohamed"
+    // setTest ({name:"peter"})
+  }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <div className="app-container">
+      
+      <button onClick={()=> setIsOpen((is)=> !is )} className="btn-close">&times;</button>
+      {/* {isOpen ?  <button onClick={()=> setIsOpen(false)} className="btn-close">&times;</button> : 
+      <button onClick={()=> setIsOpen(true)} className="btn-close">open</button> 
+      } */}
 
-      <p className="message">
-        step {step} : {messages[step - 1]}
-        {test.name}
-      </p>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <div className="buttons">
+          <p className="message">
+            step {step} : {messages[step - 1]}
+          </p>
 
+          <div className="buttons">
+            <button
+              style={{
+                backgroundColor: "rgba(121, 80, 242, 1)",
+                color: "#fff",
+              }}
+              // onClick={() => alert("Previous")}
+              // onClick={function(){return alert("previous")}}
+              // onMouseEnter={()=> console.log("enter")}
+              // onMouseLeave={()=> console.log("leave") }
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
 
-        <button
-          style={{ backgroundColor: "rgba(121, 80, 242, 1)", color: "#fff" }}
-          // onClick={() => alert("Previous")}
-          // onClick={function(){return alert("previous")}}
-          // onMouseEnter={()=> console.log("enter")}
-          // onMouseLeave={()=> console.log("leave") }
-          onClick={handlePrevious}
-          
-        >
-          Previous
-        </button>
-
-
-
-
-
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          // onClick={() => alert("next")}
-          onClick={handleNext}
-        >
-          next
-        </button>
-
-
-
-      </div>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              // onClick={() => alert("next")}
+              onClick={handleNext}
+            >
+              next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ steps/src/Counterchallange.js
@@ -0,0 +1,11 @@
+function Counterchallange() {
+//      const date = new Date ("june 21 2027") ; 
+//   date.setDate(date.getDate()+count)
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Counterchallange
