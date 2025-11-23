6d852e53399d4226efb92f5d1532bb6521c96e41
achrefmechergui <achreffcb123achref@gamil.com>
2025-11-23 20:50:07 +0100

    done

--- steps/src/Counterchallange.js
+++ steps/src/Counterchallange.js
@@ -1,12 +1,41 @@
+import { useState } from "react";
+
 function Counterchallange() {
-//      const date = new Date ("june 21 2027") ; 
-//   date.setDate(date.getDate()+count)  ///
-////// for render : {date.toDateString()}  
-    return (
-        <div>
-           <h1> counter</h1> 
-        </div>
-    )
+  const [count, setCount] = useState(0);
+  const [step , setStep] = useState (1) ; 
+  const date = new Date("November 23 2025");
+  date.setDate(date.getDate() + count);
+  return (
+    <div style={{ textAlign: "center" }}>
+
+            <div>
+        <button onClick={() => setStep((cur) => cur - 1)}>-</button>
+        <span>Step : {step}</span>
+        <button onClick={() => setStep((cur) => cur + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((cur) => cur - step)}>-</button>
+        <span>Count : {count}</span>
+        <button onClick={() => setCount((cur) => cur + step)}>+</button>
+      </div>
+
+      <span>
+        {/* <span>
+                {count === 0 && <span> Today is</span>}
+             {count > 0 && `${count} days from today is`}
+              {count < 0 && `${Math.abs(count)} days ago  is`}
+            </span> */}
+
+        {count === 0
+          ? "today is"
+          : count > 0
+          ? `${count} days from today is`
+          : `${Math.abs(count)} days ago  is`}
+      </span>
+
+      <span>{date.toDateString()}</span>
+    </div>
+  );
 }
 
-export default Counterchallange
+export default Counterchallange;
